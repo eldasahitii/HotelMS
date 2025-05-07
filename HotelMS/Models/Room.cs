1b86e9c6020b300c0d5e9d4deee88ec6792cbef5
RuvejdaJaha <rj69942@ubt-uni.net>
2025-05-07 15:05:10 +0200

    Room Table model

---
+++ HotelMS/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelMS.Models
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Capacity { get; set; }
+        public string Size { get; set; }=string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public string ImageUrl { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        //public ICollection<RoomAmenity> RoomAmenities { get;set; }
+        //public ICollection<Reservation>Reservations { get; set; }
+    }
+}
