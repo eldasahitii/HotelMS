8be96747e65ff1fba766d0d1363de55021a32c73
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-05-11 01:36:37 +0200

    added the HotelServiceScheduleService and wrote the code

---
+++ HotelMS/Controllers/HotelServiceScheduleController.cs
@@ -0,0 +1,19 @@
+﻿using HotelMS.Data.DTO;
+using HotelMS.Data.Interfaces;
+using HotelMS.Models;
+using HotelMS.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelMS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HotelServiceScheduleController : ControllerBase
+    {
+        private readonly HotelServiceScheduleService (IHotelServiceScheduleService scheduleService)
+        {
+            _scheduleService
+        }
+
+    }
+}
--- HotelMS/Services/HotelServiceScheduleService.cs
+++ HotelMS/Services/HotelServiceScheduleService.cs
@@ -1,19 +1,40 @@
-﻿//using HotelMS.Data;
-//using HotelMS.Data.DTO;
-//using HotelMS.Data.Interfaces;
-//using HotelMS.Models;
-//using Microsoft.EntityFrameworkCore;
-//using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using HotelMS.Data;
+using HotelMS.Data.DTO;
+using HotelMS.Data.Interfaces;
+using HotelMS.Models;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-//namespace HotelMS.Services
-//{
-//    public class HotelServiceScheduleService : IHotelServiceScheduleService
-//    {
-//        private readonly DataContext _context;
+namespace HotelMS.Services
+{
+    public class HotelServiceScheduleService : IHotelServiceScheduleService
+    {
+        private readonly DataContext _context;
 
-//        public HotelServiceScheduleService(DataContext context)
-//        {
-//            _context = context;
-//        }
-//    }
-//}
+        public HotelServiceScheduleService(DataContext context)
+        {
+            _context = context;
+        } 
+        public async Task <IEnumerable<HotelServiceSchedule>> GetAllSchedulesAsync ()
+        {
+            return await _context.HotelServiceSchedules
+                .Include(s => s.Service)
+                .ToListAsync();
+        }
+        public async Task <HotelServiceSchedule> CreateScheduleAsync(HotelServiceSchedule schedule)
+        {
+            _context.HotelServiceSchedules.Add(schedule);
+            await _context.SaveChangesAsync();
+            return schedule;
+        }
+        public async Task<bool> DeleteScheduleAsync(int id)
+        {
+            var schedule = await _context.HotelServiceSchedules.FindAsync(id);
+            if (schedule == null) return false;
+
+            _context.HotelServiceSchedules.Remove(schedule);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
