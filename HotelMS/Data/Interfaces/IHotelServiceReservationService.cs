94621636beadb571e10f0394f507c5e1b26fbd19
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-05-11 02:20:57 +0200

    did the interface file for IHotelServiceReservationService

---
+++ HotelMS/Data/Interfaces/IHotelServiceReservationService.cs
@@ -0,0 +1,14 @@
+﻿using HotelMS.Models;
+using HotelMS.Data.DTO;
+
+namespace HotelMS.Data.Interfaces
+{
+    public interface IHotelServiceReservationService
+    {
+        Task<IEnumerable<HotelServiceReservation>> GetAllReservationAsync();
+        Task<HotelServiceReservation> GetReservationById(int id);
+        Task<HotelServiceReservation> CreateReservationByIdAsync(HotelServiceReservation reservation);
+        Task<HotelServiceReservation> UpdateReservationAsync(int id, HotelServiceReservation updatedReservation);
+        Task<bool> DeleteReservationAsync(int id);
+    }
+}
