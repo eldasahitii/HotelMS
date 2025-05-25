c46d74c85d7b737b5f1be8ec4fe9020b6dacbd36
VelsaZemaj <zemajvelsa@gmail.com>
2025-05-25 15:16:19 +0200

    did the migartion and add it

---
+++ HotelMS/Migrations/20250525131237_AddReviewImageTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewImageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReviewImages",
+                columns: table => new
+                {
+                    ReviewImageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReviewID = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReviewImages", x => x.ReviewImageID);
+                    table.ForeignKey(
+                        name: "FK_ReviewImages_Reviews_ReviewID",
+                        column: x => x.ReviewID,
+                        principalTable: "Reviews",
+                        principalColumn: "ReviewID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReviewImages_ReviewID",
+                table: "ReviewImages",
+                column: "ReviewID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReviewImages");
+        }
+    }
+}
