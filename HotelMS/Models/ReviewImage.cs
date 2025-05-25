f70cc0e83619c0d72320233725e35420392979b6
VelsaZemaj <zemajvelsa@gmail.com>
2025-05-25 15:10:49 +0200

    add the reviewImage table for more function for my module

---
+++ HotelMS/Models/ReviewImage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelMS.Models
+{
+    public class ReviewImage
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReviewImageID { get; set; }
+
+        [Required]
+        public int ReviewID { get; set; }
+
+        [ForeignKey("ReviewID")]
+        public Review Review { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }  // Store file path or filename
+    }
+}
