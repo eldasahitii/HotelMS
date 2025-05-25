66256de671b9841a88ce984b5a2d1cfa50605462
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-05-25 21:55:12 +0200

    changed the interface aswell

--- HotelMS/Data/Interfaces/IHotelServiceService.cs
+++ HotelMS/Data/Interfaces/IHotelServiceService.cs
@@ -1,21 +1,4 @@
-﻿//using HotelMS.Models;
-//using HotelMS.Data.DTO;
-
-
-//namespace HotelMS.Data.Interfaces
-//{
-//    public interface IHotelServiceService
-//    {
-//        Task<IEnumerable<HotelService>> GetServicesByTypeAsync(string type);
-//        Task<HotelService> GetServiceByIdAsync(int id);
-//        Task<IEnumerable<HotelServiceSchedule>> GetSchedulesByServiceIdAsync(int serviceId);
-//        Task<HotelServiceReservation> ReserveServiceAsync(HotelServiceReservation reservation);
-//        Task<HotelService> UpdateServiceAsync(int id, HotelService updatedService);
-//        Task<bool> DeleteServiceAsync(int id);
-
-//    }
-//}
-using HotelMS.Data.DTO;
+﻿using HotelMS.Data.DTO;
 using HotelMS.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
