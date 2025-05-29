6a27d54458a421f56712872ea0e943f4fb21836d
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-05-29 20:54:41 +0200

    hotel service interface

--- HotelMS/Data/Interfaces/IHotelServiceService.cs
+++ HotelMS/Data/Interfaces/IHotelServiceService.cs
@@ -1,17 +1,16 @@
-﻿using HotelMS.Models;
-using HotelMS.Data.DTO;
-
+﻿using HotelMS.Data.DTO;
+using HotelMS.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HotelMS.Data.Interfaces
 {
-    public interface IHotelServiceService
+    public interface IHotelService
     {
-        Task<IEnumerable<HotelService>> GetServicesByTypeAsync(string type);
-        Task<HotelService> GetServiceByIdAsync(int id);
-        Task<IEnumerable<HotelServiceDetail>> GetSchedulesByServiceIdAsync(int serviceId);
-        Task<HotelServiceReservation> ReserveServiceAsync(HotelServiceReservation reservation);
-        Task<HotelService> UpdateServiceAsync(int id, HotelService updatedService);
-        Task<bool> DeleteServiceAsync(int id);
-
+        Task<HotelService> AddService(HotelServiceDTO request);
+        Task<HotelService> GetService(int id);
+        Task<IEnumerable<HotelService>> GetAllServices();
+        Task DeleteService(int id);
+        Task<HotelService> UpdateService(int id, HotelServiceDTO request);
     }
 }
