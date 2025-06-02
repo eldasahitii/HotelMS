7ce1d31ed05f6d76ef340ec64208362df30fe3ad
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-06-02 15:33:59 +0200

    hotel service reservation interface

--- HotelMS/Data/Interfaces/IHotelServiceReservationService.cs
+++ HotelMS/Data/Interfaces/IHotelServiceReservationService.cs
@@ -1,14 +1,26 @@
-﻿//using HotelMS.Models;
-//using HotelMS.Data.DTO;
+﻿using HotelMS.Data.DTO;
+using HotelMS.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-//namespace HotelMS.Data.Interfaces
-//{
-//    public interface IHotelServiceReservationService
-//    {
-//        Task<IEnumerable<HotelServiceReservation>> GetAllReservationsAsync();
-//        Task<HotelServiceReservation> GetReservationByIdAsync(int id);
-//        Task<HotelServiceReservation> CreateReservationAsync(HotelServiceReservation reservation);
-//        Task<HotelServiceReservation> UpdateReservationAsync(int id, HotelServiceReservation updatedReservation);
-//        Task<bool> DeleteReservationAsync(int id);
-//    }
-//}
+namespace HotelMS.Data.Interfaces
+{
+    public interface IHotelServiceReservationService
+    {
+        Task<string> MakeReservation(int userID, HotelServiceReservationDTO request, List<string> roles);
+
+        Task<IEnumerable<HotelServiceReservationDTO>> GetUserReservations(int userID);
+
+        Task<IEnumerable<HotelServiceReservationDTO>> GetAllReservations();
+
+        Task<string> CancelReservation(int reservationID, int userID, bool isAdminOrStaff = false);
+
+        Task<string> UpdateReservation(int reservationID, HotelServiceReservationUpdateDTO request, int userID, List<string> roles);
+
+        Task<string> UpdateReservationStatus(int reservationID, int statusID);
+
+        Task<string> MarkReservationCompleted(int reservationID, int userID);
+        Task<List<string>> GetTakenSlotsForDate(string date);
+
+    }
+}
