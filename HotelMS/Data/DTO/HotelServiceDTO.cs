117ff9e812c9d0e4d07328bc55e9e84e71427969
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-06-02 15:27:40 +0200

    Hotel service DTO

--- HotelMS/Data/DTO/HotelServiceDTO.cs
+++ HotelMS/Data/DTO/HotelServiceDTO.cs
@@ -1,19 +1,11 @@
-﻿//using HotelMS.Models;
-//using HotelMS.Models.DTOs;
+﻿namespace HotelMS.Data.DTO
+{
+    public class HotelServiceDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string HeroImageUrl { get; set; }
 
-//namespace HotelMS.Data.DTO
-//{
-//    public class HotelServiceDTO
-//    {
-//        public string Type { get; set; }
-//        public string Name { get; set; }
-//        public string Description { get; set; }
-//        public decimal Price { get; set; }
-//        public bool IsActive { get; set; }
 
-//        public ICollection<HotelServiceScheduleDTO> HotelServiceSchedules { get; set; }
-//        public ICollection<HotelServiceScheduleCreateUpdateDTO> HotelServiceScheduleCreateUpdates { get; set; }
-//        public ICollection<HotelServiceReservationDTO> HotelServiceReservations { get; set; }
-
-//    }
-//}
+    }
+}
