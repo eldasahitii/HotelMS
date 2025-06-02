518ba8bafbf44e605bffdcb1545f95c7129aa5a0
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-06-02 15:35:01 +0200

    hotel service interface

---
+++ HotelMS/Data/Interfaces/IHotelService.cs
@@ -0,0 +1,16 @@
+﻿using HotelMS.Data.DTO;
+using HotelMS.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelMS.Data.Interfaces
+{
+    public interface IHotelService
+    {
+        Task<HotelService> AddService(HotelServiceDTO request);
+        Task<HotelService> GetService(int id);
+        Task<IEnumerable<HotelService>> GetAllServices();
+        Task DeleteService(int id);
+        Task<HotelService> UpdateService(int id, HotelServiceDTO request);
+    }
+}
