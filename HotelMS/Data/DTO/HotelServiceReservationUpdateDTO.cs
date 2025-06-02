b133629ccb68313c1a959ef110996b16be5041ba
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-06-02 15:30:49 +0200

    hotel service reservation update Dto

---
+++ HotelMS/Data/DTO/HotelServiceReservationUpdateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelMS.Data.DTO
+{
+    public class HotelServiceReservationUpdateDTO
+    {
+        [Required]
+        public DateTime ReservationDate { get; set; }
+
+        [Required]
+        public TimeSpan StartTime { get; set; }
+
+        [Required]
+        public TimeSpan EndTime { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
