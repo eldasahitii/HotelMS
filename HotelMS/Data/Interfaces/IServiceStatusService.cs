c50415b1480fec4e34bba04abdc1b42e11ab8ee0
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-06-06 23:50:03 +0200

    added the following interface for the new models

---
+++ HotelMS/Data/Interfaces/IServiceStatusService.cs
@@ -0,0 +1,16 @@
+﻿using HotelMS.Data.DTO;
+using HotelMS.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelMS.Data.Interfaces
+{
+    public interface IServiceStatusService
+    {
+        Task<ServiceStatus> AddServiceStatus(ServiceStatusDTO request);
+        Task<ServiceStatus> GetServiceStatus(int id);
+        Task<IEnumerable<ServiceStatus>> GetAllServiceStatus();
+        Task DeleteServiceStatus(int id);
+        Task<ServiceStatus> UpdateServiceStatus(int id, ServiceStatusDTO request);
+    }
+}
