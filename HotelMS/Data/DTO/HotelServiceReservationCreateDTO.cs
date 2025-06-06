107237b5e9e0577faec79eeac2b2d71e4f4b0d85
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-06-06 23:45:51 +0200

    2 new dtos and an interface for service reservations

---
+++ HotelMS/Data/DTO/HotelServiceReservationCreateDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelMS.Data.DTO
+{
+    public class HotelServiceReservationCreateDTO
+    {
+        
+        public required int ServiceID { get; set; }
+
+        
+        public required DateTime ReservationDate { get; set; }
+
+      
+        public required string TimeSlot { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = null!;
+
+        
+        public required string LastName { get; set; } = null!;
+
+       
+        public required string Email { get; set; } = null!;
+
+       
+        public required string Phone { get; set; } = null!;
+
+        public int? CustomerUserID { get; set; }
+
+        // Optional: public int? ReceptionistID { get; set; }
+        // Optional: public string? SpecialRequests { get; set; }
+    }
+}
---
+++ HotelMS/Data/DTO/HotelServiceReservationUpdateDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelMS.Data.DTO
+{
+    public class HotelServiceReservationUpdateDTO
+    {
+        public DateTime? ReservationDate { get; set; }
+
+       
+        public string? TimeSlot { get; set; }
+
+        
+        public string? FirstName { get; set; }
+
+        
+        public string? LastName { get; set; }
+
+        
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        // Optional: public string? SpecialRequests { get; set; }
+    }
+}
