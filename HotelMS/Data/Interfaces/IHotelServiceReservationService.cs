089c9e105dbd2b025eae6b172a660b2fceea6b32
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-06-08 18:40:50 +0200

    added three new interfacse

---
+++ HotelMS/Data/Interfaces/IHotelServiceReservationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HotelMS.Data.DTO;
+
+namespace HotelMS.Services.Interfaces
+{
+    public interface IHotelServiceReservationService
+    {
+        Task<IEnumerable<HotelServiceReservationDTO>> GetAllReservationsAsync();
+        Task<HotelServiceReservationDTO?> GetReservationByIdAsync(int reservationId);
+        Task<int> CreateReservationAsync(HotelServiceReservationDTO reservationDto);
+        Task<bool> UpdateReservationAsync(HotelServiceReservationDTO reservationDto);
+        Task<bool> DeleteReservationAsync(int reservationId);
+    }
+}
